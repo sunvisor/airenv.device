e2e1728cd30c15a40781a0703b2bec9011c5f196
NAKAMURA, Hisashi <hisashi@sunvisor.net>
2021-07-24 11:41:29 +0900

    web api に log を送信

---
+++ logger.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class Logger:
+
+    def __init__(self, place):
+        self.base_url = "http://192.168.0.231/"
+        self.place = place
+        pass
+
+    def log(self, item_name, item_value):
+        url = self.base_url + 'add' + '/' + item_name + '/' + self.place + '/' + str(item_value)
+        requests.post(url)
+
+
+if __name__ == "__main__":
+    logger = Logger('test')
+    logger.log('co2', 500);
--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from time import sleep
 from ccs811 import Co2Sensor
 from led import Led
 from judge import co2_judge
-
+from logger import Logger
 
 sensor = Co2Sensor()
 led = Led()
+logger = Logger('home_office')
+count = 0
 while True:
     value = sensor.read_data()
     led.select(co2_judge(value['co2']))
-    sleep(30)
+    count = count + 1
+    if count > 20:
+        logger.log('co2', value['co2'])
+        count = 0;
+    sleep(3)
